ed6c060fa87fcbd86565a471f9e85653eb33af88
Henrik Sejer Pedersen <henrik@opgo.dk>
2019-12-27 17:32:10 +0100

    fixed CI

--- docker_push.sh
+++ docker_push.sh
@@ -30,5 +30,5 @@ then
 fi
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker tag webserver dm874/webserver:$TRAVIS_COMMIT
+docker tag $LOCAL_IMAGE dm874/webserver:$TRAVIS_COMMIT
 docker push dm874/webserver:$TRAVIS_COMMIT
